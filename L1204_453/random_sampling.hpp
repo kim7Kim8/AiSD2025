151fbf36bef47cd4afd5cd65d6f1ccd2c5209596
kim7Kim8 <106241674+kim7Kim8@users.noreply.github.com>
2025-12-05 22:36:26 +0500

    Update random_sampling.hpp

--- L1204_453/random_sampling.hpp
+++ L1204_453/random_sampling.hpp
@@ -1,32 +1,19 @@
+//еще поменяю чуть позже
+
 #ifndef RANDOM_SAMPLING_HPP
 #define RANDOM_SAMPLING_HPP
 
+// Минимальные объявления для компиляции
 typedef struct {
-    int n; // количество городов
-    // другие параметры задачи коммивояжера
+    int n;
 } tsp_instance;
 
 typedef struct {
-    int* tour; // маршрут
-    int length; // длина маршрута
+    int* tour;
+    int length;
 } tsp_solution;
 
-// Инициализация решения
-void initialize_solution(int n, tsp_solution* s);
-
-// Случайное решение
-void random_solution(tsp_solution* s);
-
-// Вычисление стоимости решения
-double solution_cost(tsp_solution* s, tsp_instance* t);
-
-// Копирование решения
-void copy_solution(tsp_solution* src, tsp_solution* dest);
-
-// Обновление счетчика решений
-void solution_count_update(tsp_solution* s, tsp_instance* t);
-
-// Основная функция произвольной выборки
-void random_sampling(tsp_instance* t, int nsamples, tsp_solution* best_solution);
+// Только функция из листинга 12.4
+void random_sampling(tsp_instance* t, int nsamples, tsp_solution* s);
 
 #endif
