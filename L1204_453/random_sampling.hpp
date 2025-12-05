bb0d4821d0875f86cd3efd2600d8bf48345f10fe
kim7Kim8 <106241674+kim7Kim8@users.noreply.github.com>
2025-12-05 22:36:54 +0500

    Update random_sampling.hpp

--- L1204_453/random_sampling.hpp
+++ L1204_453/random_sampling.hpp
@@ -3,7 +3,6 @@
 #ifndef RANDOM_SAMPLING_HPP
 #define RANDOM_SAMPLING_HPP
 
-// Минимальные объявления для компиляции
 typedef struct {
     int n;
 } tsp_instance;
@@ -13,7 +12,7 @@ typedef struct {
     int length;
 } tsp_solution;
 
-// Только функция из листинга 12.4
+// функция из листинга 12.4
 void random_sampling(tsp_instance* t, int nsamples, tsp_solution* s);
 
 #endif
